098c6a8da8e290fab536f07925e4bea0cf99428e
Markus Wanner <markus@bluegap.ch>
2024-11-28 23:34:11 +0100

    Add test parsing fbautostart control file

--- src/lib.rs
+++ src/lib.rs
@@ -16,5 +16,7 @@ pub mod control;
 pub mod version;
 
 pub use self::changelog::{Changelog, ChangelogEntry};
-pub use self::control::{ControlFile, ControlParagraph, ControlValue, ControlEntry};
-pub use self::version::Version;
+pub use self::control::{
+    ControlEntry, ControlFile, ControlParagraph, ControlValue,
+};
+pub use self::version::{Version, VersionElement, VersionPart};
